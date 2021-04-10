c35d10b99813d70335d1088a40898914d3422b11
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2021-04-10 20:57:38 +0100

    update interviews page

--- event-listeners.js
+++ event-listeners.js
@@ -1,11 +1,25 @@
 const navContainer = document.querySelector('.nav');
 
 navContainer.addEventListener('mouseover', function(element) {
-  element.target.style.color = '#343a40';
+  element.target.style.color = "#343A40";
   element.target.style.backgroundColor = "#f1f4f8";
 });
 
 navContainer.addEventListener('mouseout', function(element) {
+  element.target.style.color = '#343A40';
+  element.target.style.backgroundColor = "#FFFAFA"
+  
+});
+
+// SUMMARY CONTAINER FROM INTERVIEW PAGE
+const summaryContainer = document.querySelector('summary');
+
+summaryContainer.addEventListener('mouseover', function(element) {
+  element.target.style.color = '#fffafa';
+  element.target.style.backgroundColor = "#ff7f50";
+});
+
+summaryContainer.addEventListener('mouseout', function(element) {
   element.target.style.color = '#343A40';
   element.target.style.backgroundColor = "#FFFAFA"
   
