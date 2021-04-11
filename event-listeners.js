6a352d707c8835622df6e636a5fee88b0f1d7663
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2021-04-11 02:24:30 +0100

    updates to flexbox

--- event-listeners.js
+++ event-listeners.js
@@ -8,11 +8,12 @@ navContainer.addEventListener('mouseover', function(element) {
 navContainer.addEventListener('mouseout', function(element) {
   element.target.style.color = '#343A40';
   element.target.style.backgroundColor = "#FFFAFA"
-  
 });
 
 // SUMMARY CONTAINER FROM INTERVIEW PAGE
-const summaryContainer = document.querySelector('summary');
+
+// tried using querySelectorAll but didnt work
+let summaryContainer = document.querySelector('.summary');
 
 summaryContainer.addEventListener('mouseover', function(element) {
   element.target.style.color = '#fffafa';
@@ -22,11 +23,12 @@ summaryContainer.addEventListener('mouseover', function(element) {
 summaryContainer.addEventListener('mouseout', function(element) {
   element.target.style.color = '#343A40';
   element.target.style.backgroundColor = "#FFFAFA"
-  
 });
 
 const header = document.querySelector('.intro');
 
+
+// CODE FOR PROJECTS
 const leftBtn = document.querySelector('.left-button');
 
 leftBtn.addEventListener('click', function (element) {
